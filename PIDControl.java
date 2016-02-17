4808c93bd5e15710ff4c31316acdc910ca1183ae
Yvonne Chen <cay1025@gmail.com>
2016-02-16 16:51:48 -0800

    Update PIDControl.java

--- PIDControl.java
+++ PIDControl.java
@@ -7,7 +7,7 @@ package org.usfirst.frc.team2643.robot;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
