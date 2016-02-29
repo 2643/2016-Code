4e8576a3ea70b253d1ed0ca1b86dc34d74e769c9
Justin Case <oliverjustin6615@students.esuhsd.org>
2016-02-29 14:16:10 -0800

    Update PIDControl.java

--- PIDControl.java
+++ PIDControl.java
@@ -1,210 +1,158 @@
-/*
-Authors:
-Arman Amjad
+/*Authors:
+*Vikasni
+*Niko
 */
+
 package org.usfirst.frc.team2643.robot;
- 
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
- 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
-public class Robot extends IterativeRobot {
-   
-    final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
-    String autoSelected;
-    SendableChooser chooser;
-    //default variables
-   
-    static double powerIncrement = 0.05;
-    static double speedUpWait = 0.05;
-    static double secondWait = 0.05;
-    //UPDATE NUMBERS LATER^
-   
-   
-    static final double preset1 = 3;
-    static final double preset2 = 6;
-    static double currentRPS = 0;
-    static double currentPower = 0;
-    static double desiredRPS;
-    static double desiredPower;
-    static boolean is1Preset = false;
-    static boolean is2Preset = false;
-    //UPDATE LATER
-   
-    static Timer pidTimer = new Timer();
-    static Timer rpsTimer = new Timer();
-    static Timer timer = new Timer();
-    static Joystick stick1 = new Joystick(0);
-    static Victor shooterMotor = new Victor(4);
-    static Encoder shooterEncoder = new Encoder(4,5);
-    final int powerChangingState = 0;
-    final int waitingState = 1;
+
+public class AutoMethods extends Robot{
+	//set drive sets the two sides to a speed indep.
+    public static void setDrive(double leftSpeed,double rightSpeed){
+    	// sets the motors to a speed: the right is negative bc the motors are inverted
+    	frontRightMotor.set(-rightSpeed);	
+    	backRightMotor.set(-rightSpeed);
+    	backLeftMotor.set(leftSpeed);
+    	frontLeftMotor.set(leftSpeed);
+    }
+    //set drive sets all driving 
+    public static void setDrive(double speed){
+    	//sets the motors to a speed, right side negative bc of inverted motors
+    	frontLeftMotor.set(speed);
+        frontRightMotor.set(-speed);
+        backLeftMotor.set(speed);
+        backRightMotor.set(-speed);
+    }
     
-    int state = powerChangingState;
- //   final int calculatingRPSState = 2;
    
-    double timeInterval = 500;
-   
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("My Auto", customAuto);
-        SmartDashboard.putData("Auto choices", chooser);
-        //Default code
-       
-        shooterEncoder.reset();
-       
-       
-    }
-   
-    /**
-     * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-     * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-     * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
-     * below the Gyro
-     *
-     * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
-     * If using the SendableChooser make sure to add them to the chooser code above as well.
-     */
-    public void autonomousInit() {
-        autoSelected = (String) chooser.getSelected();
-//      autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
-        System.out.println("Auto selected: " + autoSelected);
-    }
- 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-        switch(autoSelected) {
-        case customAuto:
-        //Put custom auto code here  
-            break;
-        case defaultAuto:
-        default:
-        //Put default auto code here
-            break;
+    public static boolean moveForward(double distanceTillUp,double speed){
+    	//sets the drive to go forward , returns true or false
+                if( Math.abs(distanceTillUp) < Math.abs(leftDriveEncoder.get()) ||  Math.abs(distanceTillUp) < Math.abs(rightDriveEncoder.get())){
+                      	//if the distance  is less than the encoder, then it'll set the drive to a speed
+                	setDrive(speed);
+                      	return true;
+                }
+                return false;
         }
-    }
-    public double toPower(double rps){
-        double power;
-        if(rps < 0.4){
-            power = 0.0;
+
+   /*public static boolean crossChevalDeFrise() {
+   	//moves piston and goes forward
+        	if(leftDriveEncoder.get() >= 60 && rightDriveEncoder.get() >= 60){
+        		piston.set(false);
+        	}else{
+        		piston.set(true);
+        	}
+			return moveForward(distanceOverDefense,.5);
         }
-        else{
-        power = 3.067 - (Math.sqrt((-0.52192 * (rps +0.3)) + 9.24679));
-        //Uses equation from known data
+        */
+    
+        //all of this moves forward 
+        public static boolean crossMoat() {
+        	//moves forward at a speed
+                return moveForward(distanceOverDefense,0.5);
         }
-        return power;
-    }
-   
-    public void incrementPower(double differenceInPower){
-    	if(desiredPower > 0){
-    		if(currentPower > desiredPower){
-    			System.out.println("1");
-    			//Do not increment power
-    		}
-    		else if(differenceInPower >= powerIncrement){
-    			//Increments power to max power increment
-    			//System.out.println("2");
-    			currentPower += powerIncrement;
-    		}
-    		else{
-    			//Increases power to desired power
-    			currentPower += differenceInPower;
-      	 }
-    	}
-    if(desiredPower < 0){
-    	if(currentPower < desiredPower){
-			//Do not decrement power
-		}
-		else if(differenceInPower >= powerIncrement){
-			//Decrements power to max power increment
-			currentPower -= powerIncrement;
-		}
-		else{
-			//Decreases power to desired power
-			currentPower -= differenceInPower;
-  	 }
-    }
-    }
 
-   
-    /**
-     * This function is called periodically during operator control
-     */
-   
-    public void teleopPeriodic() {
 
-        rpsTimer.start();
-        pidTimer.start();
+        public static boolean crossRoughTerrain() {
+        	//same as above
+        	 return moveForward(distanceOverDefense,0.5);
+        }
 
-        long lastTime = System.currentTimeMillis();
-        while(isOperatorControl() && isEnabled()){
-        	//Gets revolutions every second
-            if(lastTime <= System.currentTimeMillis() + 500){
-              if(rpsTimer.get() > 0.5){
-                currentRPS = Math.abs(shooterEncoder.get()*2.0/255.0);
-                System.out.println(currentRPS);
-                rpsTimer.reset();
-                shooterEncoder.reset();
-               }
-                //PID Control
-                desiredRPS = 7.5;
-                desiredPower = toPower(desiredRPS);
+
+        public static boolean crossRamparts() {
+        	//same as above
+        	 return moveForward(distanceOverDefense,0.5);
+        }
+
+
+        public static boolean crossRockWall() {
+        	//same as above
+        	 return moveForward(distanceOverDefense,0.3);
+        }
+
+//turnMove is self explaitory it makes the robot turn then move
+        public static int turnMove(double shiftStartingPosition, int turnMoveState) {
+               	switch(turnMoveState){
+               	case 0:
+               		//turn left or right
+               		if(shiftStartingPosition > 0){ 
+               			//if the starting position you put in the slider  is greater than 0, then...
                
-                switch(state){
-                
-                
-           
-                    case powerChangingState:
-                   
-                        double powerDifference = Math.abs(desiredPower - currentPower);
-                        incrementPower(powerDifference);
-                        shooterMotor.set(currentPower);
-                        state = waitingState;
-                        break;
-                   
-                    case waitingState:
-                   
-                        if(pidTimer.get() >= (speedUpWait + secondWait)){
-                            desiredPower = (desiredRPS/currentRPS)*currentPower;
-                            //calculates desired RPS
-                            state = powerChangingState;
-                        }
-                        break;
-                   
-                    default:
-                   
-                        System.out.println("PID State Default Error!!");
-                        state = powerChangingState;
-                }//switch
-            lastTime = System.currentTimeMillis();
-            }//if
-        }//while
-    }
-   
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-   
-    }
-   
+               			if(turnedRight()){
+               				//if you turned right
+               				setDrive(.5);
+               				resetEncoders();
+               				return 1;
+               				//returns a 1 so it can go to the next state 
+               			}
+               		}else if(shiftStartingPosition < 0){
+               			// if less than zero 
+               			if(turnLeft()){
+               				setDrive(.5);
+               				resetEncoders();
+               				return 1;
+               				//returns a 1
+               			}
+               		}else{
+               			return 1;
+               		}
+               		break;
+               	case 1:
+               		//move towards the desired defense
+               		if(moveForward(shiftStartingPosition*distanceBetweenDefenses,.5)){
+               			resetEncoders();
+               			return 2;
+               		}
+               		break;
+               	case 2:
+               		//turn again
+               		if(shiftStartingPosition < 0){
+               			if(turnedRight()){
+               				setDrive(.5);
+               				resetEncoders();
+               				return 3;
+               			}
+               		}else if(shiftStartingPosition > 0){
+               			if(turnLeft()){
+               				setDrive(.5);
+               				resetEncoders();
+               				return 3;
+               			}
+               		}else{
+               			return 3;
+               		}
+               	}
+				return turnMoveState;
+        }
+
+        public static void resetEncoders(){
+        	rightDriveEncoder.reset();
+            leftDriveEncoder.reset();
+        }
+        public static boolean turnedRight() {
+                if(turn90Amount >  Math.abs(leftDriveEncoder.get()) || turn90Amount >  Math.abs(rightDriveEncoder.get())){
+                 // if the encoders are less than the turn 90 amount, then it'll turn       
+                	setDrive(.5,-.5);
+                       return(false);
+                }else{
+                	//when it's greater than the turn 90 amount, it'll return true
+                	return(true);
+                }
+        }
+        public static String getEncoders(){
+        	//gives you the encoder values
+        	return leftDriveEncoder.get() + " " + rightDriveEncoder.get();
+        }
+
+        public static boolean turnLeft() {
+        	// if the encoders are less than the turn 90 amount, then it'll turn       
+        	if(turn90Amount >  Math.abs(leftDriveEncoder.get()) || turn90Amount >  Math.abs(rightDriveEncoder.get())){
+                setDrive(-.5,.5);
+                return(false);
+         }else{
+        	//when it's greater than the turn 90 amount, it'll return true
+         
+         	return(true);
+         }
+        }
+		
 }
